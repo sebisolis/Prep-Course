625dbbc3372a8176928dc4803760328fc4a29d9d
Sebastian Solis <sebasolis@users.noreply.github.com>
2022-02-27 10:48:45 -0300

    checkpoint 02-JS-I

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -238,13 +238,22 @@ function esVocal(letra){
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
   
-  if(letra.length > 1){
+  if(letra.length > 1)
+  {
     return "Dato incorrecto";
   }
-  else if(letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u"){
+  else if(
+    letra === "a" ||
+    letra === "e" ||
+    letra === "i" ||
+    letra === "o" ||
+    letra === "u"
+    )
+  {
     return "Es vocal";
   }
-  else{
+  else
+  {
     return "Dato incorrecto";
   }
 
